6428a2c3d49d335fb2d18f9a02e2687dcf589e9f
David Ingram <david@ingramaviation.com>
2019-08-19 04:05:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,16 @@
-class Dog 
+class Dog
+ 
   def name=(dogs_name)
     @this_dogs_name = dogs_name
-  end 
-  
+  end
+ 
   def name
-    @this this_dogs_name
+    @this_dogs_name
   end
 end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
 
